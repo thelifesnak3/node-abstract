55d08b70b1ade7ec90fb6bb21a8a1410a627aa20
unknown <felipe.silva1kb@gmail.com>
2018-07-30 23:29:30 -0300

    atualizacoes

--- src/abstract_execute/Usuario.js
+++ src/abstract_execute/Usuario.js
@@ -1,9 +1,8 @@
 module.exports = app => {
     
-        const seq = require('../../config/db/postgres.js')
-        const $abs = require('../utils/Abstract.js');
-        const Usuario = require('../models/Usuario.js')(app);
+    const $abs = require('../utils/Abstract.js')
+    const Usuario = require('../src/models/Usuario.js')
+    
+    const U_abs = $abs.create(app, Usuario, `${app.usuario_route_base}`)
 
-        const U_abs = $abs.create(app, Usuario, `${app.usuario_route_base}`)
-
-    }
+}
